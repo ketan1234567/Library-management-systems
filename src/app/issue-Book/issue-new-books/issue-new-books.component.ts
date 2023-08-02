6309c27e43d92890ee7603a1da6a596f0fe32959
ketanDeshmukh <deshmukhketan647@gmail.com>
2023-08-03 05:14:17 +0530

    yes

--- src/app/issue-Book/issue-new-books/issue-new-books.component.ts
+++ src/app/issue-Book/issue-new-books/issue-new-books.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable, combineLatest, filter, forkJoin, map, of, scan, timer } from 'rxjs';
+import { SiginService } from 'src/app/services/sigin.service';
 
 @Component({
   selector: 'app-issue-new-books',
@@ -7,19 +9,43 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./issue-new-books.component.css']
 })
 export class IssueNewBooksComponent implements OnInit {
-  ngOnInit(): void {
-    
+  constructor(private services:SiginService){}
+  checkdata:any
+  stdNames$: Observable<string[]> | undefined; 
+  ngOnInit():void {
+    this.services.GetUserDetails().subscribe((result)=>{
+      //console.log(result.body);
+      
+    })
+
   }
+
 
   reactiveForm=new FormGroup({
     StudentId:new FormControl('',Validators.required),
     isbnnumber:new FormControl('',Validators.required)
 
   })
-  addIssueBooks(){
- console.log(this.reactiveForm.value);
+  addIssueBooks(){}
+allData(){
+  
+}
+  onKeyUp(event:Event):void{
+    const data=(event.target as HTMLTextAreaElement).value
+    //console.log(data);
+    this.services.GetUserDetails().pipe(
+      map((data)=>console.log(data.body[0]),
+      
+      )
+    ).subscribe((result)=>{
+      console.log(result);
+     // this.checkdata=result.body
+      //console.log(this.checkdata);
+       
+      
+    })
+    
   }
-  changeColor(event: Event){
-    console.log(event.target);
-  }
+
+  
 }
